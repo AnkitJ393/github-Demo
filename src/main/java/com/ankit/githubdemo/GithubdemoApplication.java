3807ab96eaae0eb5c96489448e5fb7324252768c
AnkitJ393 <jangidankit767@gmail.com>
2022-03-16 14:23:13 +0530

    Intial Commit

---
+++ src/main/java/com/ankit/githubdemo/GithubdemoApplication.java
@@ -0,0 +1,14 @@
+package com.ankit.githubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubdemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GithubdemoApplication.class, args);
+	}
+
+}
